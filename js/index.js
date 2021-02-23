54c8221993a4277f896ac0d5d28a82787857a6bf
Christopher-Robin <cebbinghaus@live.de>
2021-02-23 11:02:16 +1100

    Added URL Query Parameters

--- js/index.js
+++ js/index.js
@@ -2,5 +2,15 @@ var g;
 window.directions = [[-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1]]
 
 window.onload = () => {
-	g = new game("Canvas", 10, 2)
+	const urlParams = new URLSearchParams(window.location.search);
+
+	let fieldSize = Number.parseInt(urlParams.get('FieldSize'));
+	if(isNaN(fieldSize))
+		fieldSize = 10;
+
+	let percentage = Number.parseInt(urlParams.get('Percentage'));
+	if(isNaN(percentage))
+		percentage = 20;
+
+	g = new game("Canvas", fieldSize, percentage)
 }
